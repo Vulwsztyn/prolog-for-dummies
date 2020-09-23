3fb75d415b927ee5c323a4009224a9a0e8a13ab3
Artur Mostowski <artur.mostowski@currency-one.com>
2020-09-23 08:51:38 +0200

    sorting algorithms

---
+++ sorting_algorithms/bubble_sort.pl
@@ -0,0 +1,37 @@
+% bubble(+CurrentMax, +List, -ListWithoutMax, -Max)
+% i think the parameters are self-explanatory here
+% ! needed, because input args are the same in 2nd and 3rd line
+bubble(X,[],[],X).
+bubble(X,[Y|T],[Y|NT],Max) :- X>Y, bubble(X,T,NT,Max), !.
+bubble(X,[Y|T],[X|NT],Max) :- X=<Y, bubble(Y,T,NT,Max).
+
+% notice that the third line could be 
+% bubble(X,[Y|T],[X|NT],Max) :- bubble(Y,T,NT,Max).
+% because if X>Y it gets stopped by the ! in the 2nd line
+
+% it is run as in 
+% b_sort([H|T],Acc,Sorted) :- bubble(H,T,NT,Max), b_sort(NT,[Max|Acc],Sorted), !.
+% meaning that the current max is the element of the list upon which the bubbling is performed
+
+test_bubble(N,M) :- bubble(3,[18,  6,  5,  2,  8, 15, 13, 14,  0, 12,  7,  9, 10, 19, 16, 11],N,M).
+% M = 19,
+% N = [3, 6, 5, 2, 8, 15, 13, 14, 0, 12, 7, 9, 10, 18, 16, 11]
+
+
+% b_sort(+List, +SortedAccumulator, -ListMergedWithAcc)
+% prepends the accumulator with maximal element of the list and runs self again without the max element
+% notice that in oder to work properly to accumulator needs to be sorted and all elements of the accumulator need to be greater or equal to max(List), hence the name
+% ! needed - found by trial and error, I will explain this when I get it
+b_sort([],Acc,Acc).
+b_sort([H|T],Acc,Sorted) :- bubble(H,T,NT,Max), b_sort(NT,[Max|Acc],Sorted), !.
+
+test_b_sort(S) :- b_sort([ 3, 6, 5, 2, 8, 15, 13, 14, 0, 12, 7, 9, 10, 11, 1, 4],[16,17,18,19],S).
+% S = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+
+% bubble_sort(+List, -Sorted)
+% runs the b_sort with [] as accumulator
+bubble_sort(List,Sorted):-b_sort(List,[],Sorted).
+
+test_bubble_sort(S).
+% S = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
---
+++ sorting_algorithms/quick_sort.pl
@@ -0,0 +1,41 @@
+% pivoting(+Pivot, +List, -Lesser, -Greater)
+% returns divides the list into 2 lists one contating elements lesser than (or equal to) the pivot and the other greater
+% ! is not needed as there is no differance in the input parameters of the predicate 
+%   and the value check is performed after "going into" the predicates body
+pivoting(_,[],[],[]) :- !.
+pivoting(P,[H|T],[H|L],G) :- H=<P, pivoting(P,T,L,G), !.
+pivoting(P,[H|T],L,[H|G]) :- H>P, pivoting(P,T,L,G).
+% lista[0]
+% lista[1:]
+% to better explain that the order of lines defines where to put the !
+% here is the same predicate with the second and third line switched
+% pivoting(_,[],[],[]).
+% pivoting(H,[X|T],L,[X|G]) :- X>H, pivoting(H,T,L,G), !.
+% pivoting(H,[X|T],[X|L],G) :- X=<H, pivoting(H,T,L,G).
+
+test_pivoting(L,G) :- pivoting(10,[ 3, 18, 6, 5, 2, 8, 15, 13, 14, 0, 12, 7, 9, 10, 19, 16, 11, 1, 4, 17],L,G).
+% G = [18, 15, 13, 14, 12, 19, 16, 11, 17]
+% L = [3, 6, 5, 2, 8, 0, 7, 9, 10, 1, 4]
+
+% as you can see the pivtiong preserves order which has no implications to us, 
+% but is logical when you understand the implementation
+
+
+% quick_sort(+List, -SortedList)
+% sorts the list
+% first it chooses the pivot (first element) 
+% then it divides the rest of the list into elements 2 lists (less or equal than the pivot and the rest) using pivoting predicate
+% then it recursively sorts both lists
+% finally it concatenates the sorted lists with pivot inbetween 
+% (as we know it is greater than or equal to the greates elements of the first list and less than the least element of the second list)
+% both ! are needed I'm not now  sure why but I checked it and the interpreter tried to find other fitting options
+quick_sort([],[]) :- !.
+quick_sort([H|T],Sorted):-
+    pivoting(H,T,L,G), 
+    quick_sort(L,SortedL), 
+    quick_sort(G,SortedG),
+    append(SortedL,[H|SortedG], Sorted),
+    !.
+
+test_quick_sort(S) :- quick_sort([ 3, 18,  6,  5,  2,  8, 15, 13, 14,  0, 12,  7,  9, 10, 19, 16, 11, 1, 4, 17], S). 
+% S = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
